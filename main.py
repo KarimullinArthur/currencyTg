04a7aab4c3eb96f7c3099394b352c0d8e46db8e6
Arthur <KarimullinArthur@engineer.com>
2022-05-18 21:44:31 +0500

    Хендлеры

--- main.py
+++ main.py
@@ -15,11 +15,43 @@ def menu(message):
     chat_id = message.chat.id
     text = message.text
 
-    if text == markup.buttonRuToUz:
-        bot.send_message(chat_id,message)
+    if text == markup.rub:
+        msg = bot.send_message(chat_id,getRate('RUB'),reply_markup=markup.menuRub)
+        bot.register_next_step_handler(msg,menuRub)
 
-    elif text == markup.buttonUzToRu:
-        bot.send_message(chat_id,message)
+    elif text == markup.usd:
+        msg = bot.send_message(chat_id,getRate('USD'),reply_markup=markup.menuUsd)
+
+        bot.register_next_step_handler(msg,menuUsd)
+
+def menuRub(message):
+    chat_id = message.chat.id
+    text = message.text
+
+    if text == markup.buttonUzToRub or text == markup.buttonUzToRub:
+        pass
+
+    elif text == markup.cancel:
+        msg = bot.send_message(chat_id,text,reply_markup=markup.menu)
+        bot.register_next_step_handler(msg,menu)
+
+def menuUsd(message):
+    chat_id = message.chat.id
+    text = message.text
+
+    if text == markup.buttonUzToUsd or text == markup.buttonUsdToUz:
+        msg = bot.send_message(chat_id,"Введите сумму",reply_markup=markup.menuCencel)
+        bot.register_next_step_handler(msg,amount) 
+
+    elif text == markup.cancel:
+        msg = bot.send_message(chat_id,text,reply_markup=markup.menuCencel)
+        bot.register_next_step_handler(msg,menu)
+
+def amount(message):
+    chat_id = message.chat.id
+    text = message.text
+
+    
 
 print('Run!')
 bot.infinity_polling()
--- markup.py
+++ markup.py
@@ -1,8 +1,26 @@
 import telebot
 from telebot import types
 
-buttonUzToRu = "Сум в рубль"
-buttonRuToUz = "Рубль в сум"
+rub = 'Рубль'
+usd = 'Доллар'
+cancel = 'Назад'
 
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-menu.row(buttonUzToRu,buttonRuToUz)
+menu.row(rub,usd)
+
+buttonUzToRub = "Сум в рубль"
+buttonRubToUz = "Рубль в сум"
+
+menuRub = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menuRub.row(buttonUzToRub,buttonRubToUz)
+menuRub.row(cancel)
+
+buttonUzToUsd = "Сум в доллар"
+buttonUsdToUz = "Доллар в сум"
+
+menuUsd = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menuUsd.row(buttonUzToUsd,buttonUsdToUz)
+menuUsd.row(cancel)
+
+menuCancel = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menuCancel.row(cencel)
